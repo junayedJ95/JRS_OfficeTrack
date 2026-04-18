3290de2ca41f31cdce9f7b933bb25395c2a3479d
junayedJ95 <junayedpsrl@gmail.com>
2026-04-18 22:46:05 +0600

    Add ledger, logout and index redirect - Junayed

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-require_once 'includes/db.php';
-echo "Database connected successfully! JRS_OfficeTrack is ready. ";
+header("Location: login.php");
+exit();
 ?>
